73c6f821cb7894136cd8f5acc891eb3d45bfc9ef
astronarh <astronarh@gmail.com>
2021-08-04 14:03:41 +0300

    start

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,5 +0,0 @@
-
-7
-pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
-a
-1src/main/webapp/WEB-INF/applicationContextMVC.xml,6/c/6c2f962d9aebdca1c44ecb12d37e322f6b2c79d4
--- src/main/java/ru/astronarh/HelloController.java
+++ src/main/java/ru/astronarh/HelloController.java
@@ -3,7 +3,7 @@ package ru.astronarh;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@Controller("spring_mvc_app_one_war_exploded/")
 public class HelloController {
     @GetMapping("/hello-world")
     public String sayHello() {
